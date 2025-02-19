d7fc454eb870765763b3f7531a881e6101c5a41d
AlphaX-Qortal <olaf.wasilewski@gmx.de>
2025-02-19 13:24:21 +0100

    Added tryConnection

--- src/org/hsqldb/jdbc/JDBCPool.java
+++ src/org/hsqldb/jdbc/JDBCPool.java
@@ -175,6 +175,47 @@ public class JDBCPool
         return getConnection();
     }
 
+    /**
+     * Tries to retrieve a new connection using the properties that have already been
+     * set.
+     *
+     * @return  a connection to the data source, or null if no spare connections in pool
+     * @exception SQLException if a database access error occurs
+     */
+    public Connection tryConnection() throws SQLException {
+        for (int i = 0; i < states.length(); i++) {
+            if (states.compareAndSet(i, RefState.available, RefState.allocated)) {
+                JDBCPooledConnection pooledConnection = connections[i];
+
+                if (pooledConnection == null)
+                    // Probably shutdown situation
+                    return null;
+
+                return pooledConnection.getConnection();
+            }
+
+            if (states.compareAndSet(i, RefState.empty, RefState.allocated)) {
+                try {
+                    JDBCPooledConnection pooledConnection = (JDBCPooledConnection) source.getPooledConnection();
+
+                    if (pooledConnection == null)
+                        // Probably shutdown situation
+                        return null;
+
+                    pooledConnection.addConnectionEventListener(this);
+                    pooledConnection.addStatementEventListener(this);
+                    connections[i] = pooledConnection;
+
+                    return pooledConnection.getConnection();
+                } catch (SQLException e) {
+                    states.set(i, RefState.empty);
+                }
+            }
+        }
+
+        return null;
+    }
+
     // ------------------------- JDBC 4.0 -----------------------------------
     // ------------------- java.sql.Wrapper implementation ---------------------
 
